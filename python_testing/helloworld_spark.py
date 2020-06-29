bbcea9fe0a625134e09fb251df4579af81edbdec
syedsaadahmed <syedsaadahmed2094@yahoo.com>
2020-06-29 02:37:31 +0200

    script updated for precprocessing

--- python_testing/helloworld_spark.py
+++ python_testing/helloworld_spark.py
@@ -1,6 +1,31 @@
-from pyspark import SparkContext
+import pyspark
+from pyspark.sql.session import SparkSession
+from pyspark.sql.functions import desc
 
-sc = SparkContext('local', 'hello world')
-sc.setLogLevel("ERROR")
 
-print("Hello World!")
+sc = pyspark.SparkContext('local[*]')
+spark = SparkSession(sc)
+
+
+csv_data = spark.read.format('csv').options(header='true').load('/home/saad/Documents/SPARK/Apache-Spark-Project/python_testing/time_series_19-covid-Confirmed_archived_0325.csv')
+
+#dataa.printSchema()
+#csv_data.show(3,vertical=True)
+
+df_Australia = csv_data.na.fill(0)
+
+country = ['Australia']
+df_Australia = df_Australia.filter(df_Australia['Country/Region'].isin(country))
+
+
+columns_to_drop = ['Province/State', 'Lat', 'Long']
+df_Australia = df_Australia.drop(*columns_to_drop)
+
+
+
+exprs = {x: "sum" for x in df_Australia.columns}
+df_Australia = df_Australia.groupBy("Country/Region").agg(exprs)
+
+
+df_Australia.show(3,vertical=True)
+
