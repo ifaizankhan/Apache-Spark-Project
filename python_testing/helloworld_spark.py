7bea6512f1348dff080b24777aca143afc640a9c
Faizan khan <faizanpervaizkhan@gmail.com>
2020-07-06 18:09:17 +0200

    Data preprocessing issue resolved

--- python_testing/helloworld_spark.py
+++ python_testing/helloworld_spark.py
@@ -8,7 +8,7 @@ from pyspark.sql.types import *
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model
-
+from functools import reduce
 
 sc = pyspark.SparkContext('local')
 spark = SparkSession(sc)
@@ -28,15 +28,8 @@ df_Australia = df_Australia.filter(df_Australia['Country/Region'].isin(country))
 columns_to_drop = ['Province/State', 'Lat', 'Long']
 df_Australia = df_Australia.drop(*columns_to_drop)
 
-
-
 df_Australia = df_Australia.select(df_Australia.columns[0:1] + df_Australia.columns[11:])
 
-exprs = {x: "sum" for x in df_Australia.columns}
-df_Australia = df_Australia.groupBy('Country/Region').agg(exprs)
-
-
-df_Australia = df_Australia.drop('sum(Country/Region)')
 
 
 newNames = []
@@ -44,84 +37,110 @@ for count,ele in enumerate(df_Australia.columns,-1):
     if(count==-1):
         newNames.append('Index')
         continue
-    newNames.append(count)
-
-for c, n in zip(df_Australia.columns[0:], newNames):
-    df_Australia=df_Australia.withColumnRenamed(c, str(n))
+    newNames.append(str(count))
 
 
-df_Australia = df_Australia.toPandas().set_index("Index").transpose() 
-df_Australia['Dates'] = df_Australia.index # New column for Dates
-df_Australia = df_Australia.rename(columns={"Australia": "Cases"})
+oldColumns = df_Australia.schema.names
+# print(oldColumns)
+# print("!!!!!!!!!!!!")
+# print(newNames)
+
+df = reduce(lambda df_Australia, idx: df_Australia.withColumnRenamed(oldColumns[idx], newNames[idx]), range(len(oldColumns)), df_Australia)
+# df.printSchema()
+#df.show()
+
+# df_Australia.show()
+# df_Australia = df_Australia.drop('sum(Country/Region)')
 
 
-
-# making schema for converting back to pyspark dataframe
-mySchema = StructType([ StructField("Cases", DoubleType(), True)\
-                        ,StructField("Dates", StringType(), True)\
-                       ])
-# converting back to pyspark dataframe
-df_Australia = sql.createDataFrame(df_Australia,mySchema)
-df_Australia = df_Australia.withColumn("Dates", df_Australia["Dates"].cast(IntegerType()))
+#df.groupby('Country/Region').sum().show()
 
 
-
-from pyspark.ml.feature import VectorAssembler
-
-vectorAssembler = VectorAssembler(inputCols = ['Dates'], outputCol = 'features')
-vdf_Australia = vectorAssembler.transform(df_Australia)
-vdf_Australia = vdf_Australia.select(['features', 'Cases'])
-#vdf_Australia.show()
-
+exprs = {x: "sum" for x in df.columns}
+#print(exprs)
+df = df.groupBy('Index').agg(exprs)
+df_Australia = df
+#print(type(df))
+df.show(vertical=True)
 
 
-splits = vdf_Australia.randomSplit([0.965, 0.035])
-train_df = splits[0]
-#test_df = splits[1]
-
-# train_df = vdf_Australia.limit(50)
-test_df = vdf_Australia.orderBy(desc("features")).limit(2)
+# for c, n in zip(df_Australia.columns[:], newNames):
+#     df_Australia=df_Australia.withColumnRenamed(c, str(n))
 
 
-# train_df.show()
-# print('---------------------------------------------------------')
-# test_df.show()
-
-from pyspark.ml.regression import LinearRegression
-lr = LinearRegression(featuresCol = 'features', labelCol='Cases', maxIter=2000, regParam=0.3, elasticNetParam=0.8)
-lr_model = lr.fit(train_df)
-print("Coefficients: " + str(lr_model.coefficients))
-print("Intercept: " + str(lr_model.intercept))
+# df_Australia = df_Australia.toPandas().set_index("Index").transpose() 
+# df_Australia['Dates'] = df_Australia.index # New column for Dates
+# df_Australia = df_Australia.rename(columns={"Australia": "Cases"})
 
 
 
-trainingSummary = lr_model.summary
-print("RMSE: %f" % trainingSummary.rootMeanSquaredError)
-print("r2: %f" % trainingSummary.r2)
 
-train_df.describe().show()
-
-
-lr_predictions = lr_model.transform(test_df)
-lr_predictions.select("prediction","Cases","features").show()
-
-from pyspark.ml.evaluation import RegressionEvaluator
-lr_evaluator = RegressionEvaluator(predictionCol="prediction", \
-                 labelCol="Cases",metricName="r2")
-
-print("R Squared (R2) on test data = %g" % lr_evaluator.evaluate(lr_predictions))
+# # making schema for converting back to pyspark dataframe
+# mySchema = StructType([ StructField("Cases", DoubleType(), True)\
+#                         ,StructField("Dates", StringType(), True)\
+#                        ])
+# # converting back to pyspark dataframe
+# df_Australia = sql.createDataFrame(df_Australia,mySchema)
+# df_Australia = df_Australia.withColumn("Dates", df_Australia["Dates"].cast(IntegerType()))
 
 
 
-test_result = lr_model.evaluate(test_df)
+# from pyspark.ml.feature import VectorAssembler
 
-print("Root Mean Squared Error (RMSE) on test data = %g" % test_result.rootMeanSquaredError)
+# vectorAssembler = VectorAssembler(inputCols = ['Dates'], outputCol = 'features')
+# vdf_Australia = vectorAssembler.transform(df_Australia)
+# vdf_Australia = vdf_Australia.select(['features', 'Cases'])
+# #vdf_Australia.show()
 
 
-print("numIterations: %d" % trainingSummary.totalIterations)
-print("objectiveHistory: %s" % str(trainingSummary.objectiveHistory))
-trainingSummary.residuals.show()
 
-predictions = lr_model.transform(test_df)
-predictions.select("prediction","Cases","features").show()
+# splits = vdf_Australia.randomSplit([0.965, 0.035])
+# train_df = splits[0]
+# #test_df = splits[1]
+
+# # train_df = vdf_Australia.limit(50)
+# test_df = vdf_Australia.orderBy(desc("features")).limit(2)
+
+
+# # train_df.show()
+# # print('---------------------------------------------------------')
+# # test_df.show()
+
+# from pyspark.ml.regression import LinearRegression
+# lr = LinearRegression(featuresCol = 'features', labelCol='Cases', maxIter=2000, regParam=0.3, elasticNetParam=0.8)
+# lr_model = lr.fit(train_df)
+# print("Coefficients: " + str(lr_model.coefficients))
+# print("Intercept: " + str(lr_model.intercept))
+
+
+
+# trainingSummary = lr_model.summary
+# print("RMSE: %f" % trainingSummary.rootMeanSquaredError)
+# print("r2: %f" % trainingSummary.r2)
+
+# train_df.describe().show()
+
+
+# lr_predictions = lr_model.transform(test_df)
+# lr_predictions.select("prediction","Cases","features").show()
+
+# from pyspark.ml.evaluation import RegressionEvaluator
+# lr_evaluator = RegressionEvaluator(predictionCol="prediction", \
+#                  labelCol="Cases",metricName="r2")
+
+# print("R Squared (R2) on test data = %g" % lr_evaluator.evaluate(lr_predictions))
+
+
+
+# test_result = lr_model.evaluate(test_df)
+
+# print("Root Mean Squared Error (RMSE) on test data = %g" % test_result.rootMeanSquaredError)
+
+
+# print("numIterations: %d" % trainingSummary.totalIterations)
+# print("objectiveHistory: %s" % str(trainingSummary.objectiveHistory))
+# trainingSummary.residuals.show()
+
+# predictions = lr_model.transform(test_df)
+# predictions.select("prediction","Cases","features").show()
 
